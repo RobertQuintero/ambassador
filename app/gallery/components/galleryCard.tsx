a2cc39f1a61cca47fa45f22a8e523c58cfda3b7f
RobertQuintero <90501105+RobertQuintero@users.noreply.github.com>
2024-02-19 10:32:14 +0800

    Update galleryCard.tsx

--- app/gallery/components/galleryCard.tsx
+++ app/gallery/components/galleryCard.tsx
@@ -1,14 +1,10 @@
 "use client";
 import React, { useState } from "react";
 import { GalleryType } from "@/types/galleryType";
-import {
-  Image,
-  Modal,
-  ModalContent,
-  useDisclosure,
-} from "@nextui-org/react";
+import { Image, Modal, ModalContent, useDisclosure } from "@nextui-org/react";
 import { AnimatedCarousel } from "@/components/animation/animatedCarousel";
 import { motion } from "framer-motion";
+import { paragraph, title } from "@/components/primitives";
 
 type GalleryCardProps = {
   gallery: GalleryType;
@@ -18,12 +14,13 @@ const GalleryCard = ({ gallery }: GalleryCardProps) => {
   const { isOpen, onOpen, onClose } = useDisclosure();
   return (
     <React.Fragment>
-      <div className="flex flex-col relative max-w-xl">
-                    <motion.div onClick={onOpen}
-              className="cursor-pointer"
-              whileHover={{ scale: 1.1,zIndex: 50}}
-              whileTap={{ scale: 0.9,zIndex: 50}}
-            >
+      <div className="flex flex-col  relative max-w-xl">
+        <motion.div
+          onClick={onOpen}
+          className="cursor-pointer group overflow-hidden relative"
+          whileHover={{ scale: 1.1, zIndex: 50 }}
+          whileTap={{ scale: 0.9, zIndex: 50 }}
+        >
           <Image
             src={gallery.galleryImages[0].image}
             alt={gallery.title}
@@ -32,7 +29,24 @@ const GalleryCard = ({ gallery }: GalleryCardProps) => {
             radius="none"
             className="rounded-none"
           />
-          </motion.div>
+        <div className="flex flex-col items-center justify-center absolute z-10 top-[100vh] group-hover:top-0 h-full w-full bg-default-100/60 animate duration-1000 group-hover:duration-1000 ease-in-out group-hover:ease-in-out  ">          <div className="flex flex-col gap-2  ">
+            <span
+              className={`!font-semibold text-center capitalize text-default-900 ${title(
+                { size: "lg" }
+              )}`}
+            >
+              {gallery.title}
+            </span>
+            <span
+              className={`text-center  ${paragraph(
+                { size: "xs" }
+              )}`}
+            >
+              {gallery.description}
+            </span>
+          </div></div>
+        </motion.div>
+
         <Modal
           isOpen={isOpen}
           onClose={onClose}

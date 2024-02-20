ffa56b3dea05d82cf30a5238ba6198c8eef96363
RobertQuintero <90501105+RobertQuintero@users.noreply.github.com>
2024-02-20 21:10:58 +0800

    Update page.tsx

--- app/(home)/page.tsx
+++ app/(home)/page.tsx
@@ -8,18 +8,7 @@ export default async function Home() {
 			<p>Still on progress</p>
 			<ul className="flex flex-col">
 				<li>home</li>
-				<li>about</li>
 				<li>booking</li>
-			</ul>
-			<p>Done but still need somethings</p>
-			<ul className="flex flex-col">
-				<li>barber</li>
-				<li>salon</li>
-				<li>tattoo</li>
-				<li>branches</li>
-				<li>gallery</li>
-				<li>artist</li>
-				<li>contact</li>
 			</ul>
 
 		</React.Fragment>

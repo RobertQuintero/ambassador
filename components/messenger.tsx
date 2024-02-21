084f88a348b9502848c6ffa37f69489bc0f0d7e4
RobertQuintero <90501105+RobertQuintero@users.noreply.github.com>
2024-02-22 07:04:17 +0800

    Update messenger.tsx

--- components/messenger.tsx
+++ components/messenger.tsx
@@ -1,20 +1,38 @@
 "use client";
-import { FacebookProvider, CustomChat } from "react-facebook";
+import Script from "next/script";
+import React from "react";
 
 const Messenger = () => {
   return (
-    <FacebookProvider
-      appId="929338348839637
-"
-      chatSupport
-    >
-      <CustomChat
-        pageId="246846501842903
-"
-        themeColor="#000000"
-        minimized={true}
-      />
-    </FacebookProvider>
+    <React.Fragment>
+        <div id="fb-root"></div>
+<div id="fb-customer-chat" className="fb-customerchat">
+
+</div>
+
+<Script id="fb-chat" strategy="lazyOnload">
+{`
+var chatbox = document.getElementById('fb-customer-chat');
+chatbox.setAttribute("page\_id", "246846501842903");
+chatbox.setAttribute("attribution", "biz\_inbox");
+
+window.fbAsyncInit = function() {
+FB.init({
+xfbml            : true,
+version          : 'v12.0'
+});
+};
+
+(function(d, s, id) {
+var js, fjs = d.getElementsByTagName(s)\[0\];
+if (d.getElementById(id)) return;
+js = d.createElement(s); js.id = id;
+js.src = 'https://connect.facebook.net/en\_US/sdk/xfbml.customerchat.js';
+fjs.parentNode.insertBefore(js, fjs);
+}(document, 'script', 'facebook-jssdk'));
+`}
+</Script>
+    </React.Fragment>
   );
 };
 
